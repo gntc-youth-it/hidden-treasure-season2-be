d3231f798e8dedd420991ce273f6dc850d418a43
StoneHee99 <kiit0901@gmail.com>
2025-05-10 13:38:11 +0900

    init: initial project

---
+++ src/test/java/com/hidden_treasure_season2/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hidden_treasure_season2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
